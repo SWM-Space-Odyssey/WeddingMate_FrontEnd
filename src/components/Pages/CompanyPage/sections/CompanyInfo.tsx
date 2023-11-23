3fa1c2afc89e808343492d4714d244e2c03b7d79
CSKIM <rlackstjq987@gmail.com>
2023-11-23 17:41:26 +0900

    feat: Page Auth 체크 추가

--- src/components/Pages/CompanyPage/sections/CompanyInfo.tsx
+++ src/components/Pages/CompanyPage/sections/CompanyInfo.tsx
@@ -18,7 +18,13 @@ const CompanyInfo = (props: Props) => {
     <div className='flex px-4'>
       <div>
         {/* 이미지 들어갈 자리 */}
-        <div className='h-[88px] w-[88px]'></div>
+        <div className='h-[88px] w-[88px]'>
+          <img
+            className='h-full w-full object-cover rounded-lg'
+            src={repImgUrl}
+            alt='company representative image'
+          />
+        </div>
       </div>
       <div className='flex flex-col justify-around'>
         <div className='font-bold leading-tight mb-1.5'>{title}</div>
